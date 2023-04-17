ae8b0e7d29ea946dc8ccfec8979da51c9ec69528
Karpenko21 <irinakrivosheeva55@gmail.com>
2023-04-17 12:50:07 +0300

    Commit

--- samurai-way-main1/src/App.tsx
+++ samurai-way-main1/src/App.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React from "react";
 import './App.css';
+import Accordion from "./components/Accordion/accordion";
+import Rating from "./components/Rating/rating";
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+
+const App = () => {
+    console.log("App is rendering.");
+    return (
+        <div>
+            <PageTitle title={'This is App component'}/>
+            <PageTitle title={'Friends'}/>
+            Article 1
+            <Rating value={3}/>
+            <Accordion titleValue={"Menu"} collapsed ={true} />
+            <Accordion titleValue={"Users"} collapsed={false}/>
+            Article 2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+        </div>
+    );
 }
 
+
+type PageTitlePropsType = {
+    title: string;
+}
+function PageTitle(props: PageTitlePropsType) {
+    debugger;
+    console.log("AppTitle is rendering.");
+    return (
+        <h1>{props.title}</h1>
+    );
+}
+
+
 export default App;
+
