a44e24ce7208d27dc21b9242f34ac8d5900f3a18
Karpenko21 <irinakrivosheeva55@gmail.com>
2023-04-28 06:19:35 +0300

    lesson 12

--- samurai-way-main1/src/App.tsx
+++ samurai-way-main1/src/App.tsx
@@ -1,40 +1,18 @@
 import React from "react";
 import './App.css';
-import Accordion from "./components/Accordion/accordion";
-import Rating from "./components/Rating/rating";
-
+import {Header} from "./components/Header";
+import {NavBar} from "./components/Navbar";
+import {Profile} from "./components/Profile";
 
 const App = () => {
-    console.log("App is rendering.");
     return (
-        <div>
-            <PageTitle title={'This is App component'}/>
-            <PageTitle title={'Friends'}/>
-            Article 1
-            <Rating value={3}/>
-            <Accordion titleValue={"Menu"} collapsed ={true} />
-            <Accordion titleValue={"Users"} collapsed={false}/>
-            Article 2
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+        <div className="app-wrapper">
+            <Header/>
+            <NavBar/>
+            <Profile/>
         </div>
-    );
-}
-
-
-type PageTitlePropsType = {
-    title: string;
-}
-function PageTitle(props: PageTitlePropsType) {
-    debugger;
-    console.log("AppTitle is rendering.");
-    return (
-        <h1>{props.title}</h1>
-    );
+    )
+        ;
 }
 
 
---
+++ samurai-way-main1/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://it-incubator.io/_next/static/media/logo.8a063c2a.svg"/>
+        </header>
+    )
+}
