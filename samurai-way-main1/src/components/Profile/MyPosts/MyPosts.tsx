029234e8ec42d14190e3f95a6fd273bf076ddce8
Karpenko21 <irinakrivosheeva55@gmail.com>
2023-05-12 12:16:40 +0300

    Lesson 23

--- samurai-way-main1/src/components/Profile/MyPosts/MyPosts.tsx
+++ samurai-way-main1/src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import s from "./MyPosts.module.css";
 import {Post} from "./Posts/Post";
 
-export  const MyPosts = () => {
+export const MyPosts = () => {
     return (
-        <div className={s.posts}>
-            My posts
+        <div className={s.postsBlock}>
+            <h3>My posts</h3>
             <div className={s.item}>
-                <textarea></textarea>
-                <button>Add post</button>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <button>Add post</button>
+                </div>
             </div>
-            <div className={s.post}>
+            <div className={s.posts}>
                 <Post message="Hi!" likesCount='5'/>
                 <Post message="It's my first post" likesCount='17'/>
                 <div>
