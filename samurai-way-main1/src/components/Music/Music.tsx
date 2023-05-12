bfba60a1fe9f316d18da57a76c997d37b0ea4a73
Karpenko21 <irinakrivosheeva55@gmail.com>
2023-05-12 08:33:10 +0300

    Lesson 19

---
+++ samurai-way-main1/src/components/Music/Music.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import s from './Music.module.css'
+
+type MusicPropsType = {
+
+}
+
+
+export const Music = (props: MusicPropsType) => {
+    return (
+        <div>
+            Music
+        </div>
+    );
+};
+
